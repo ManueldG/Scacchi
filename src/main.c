8a9f89621b78c02b783a06c794231a7732b9f58c
Manuel della Gala <74384683+ManueldG@users.noreply.github.com>
2025-07-24 16:55:16 +0200

    check pezzi da completare

--- src/main.c
+++ src/main.c
@@ -19,6 +19,12 @@ int checkTorre(int [SIZE][SIZE], int ,int ,int ,int );
 
 int checkAlfiere(int [SIZE][SIZE], int ,int ,int ,int );
 
+int checkCavallo(int [SIZE][SIZE],int ,int ,int ,int );
+
+int checkRe(int [SIZE][SIZE],int ,int ,int ,int );
+
+int checkPedone(int [SIZE][SIZE],int ,int ,int ,int );
+
 /*
 TODO - matrice pezzi da rappresentare su scacchiera
 
@@ -30,7 +36,10 @@ e impedire di mangiare i propri pezzi
 
 DONE impedire di spostare i pezzi dell'avversario
 
-TODO mosse come creare un pattern?per ora mosse libere 
+DONE controllo casella di partenza e di arrivo se c'è un pezzo
+caso d'errore -spostamento o cattura
+
+IN PROGRESS mosse come creare un pattern?per ora mosse libere (torre alfiere cavallo regina)(Re pedone da fare) 
 
 IN PROGRESS validazione mosse
 
@@ -40,8 +49,8 @@ TODO arrocco (lungo corto)
 
 TODO cronologia lista mosse
 
-DONE controllo casella di partenza e di arrivo se c'è un pezzo
-caso d'errore -spostamento o cattura
+TODO errori appaiono tutti insieme
+
 */ 
 
 int main(int argc, char *argv[]){    
@@ -57,17 +66,34 @@ int main(int argc, char *argv[]){
     init( board ); //
     initOut(out); //  initialize board and out <init.c>
 
-    printf( "\e[1;1H\e[2J"); //CLS
+    //printf( "\e[1;1H\e[2J"); //CLS
     printMatrice(board);
 
     for(int x = 0 ; x < TURNI ; x++){
         
         do{
             flag = 0;
+            
             printf("%s inserisci pos iniziale x y: ",(turno ? PLR_TWO: PLR_ONE));
-            scanf("%d %d",&x1,&y1);
+            scanf("%c %d",&x1,&y1);
+            fflush(stdin);
 
-            if( (identificaPezzo(board[x1][y1])[1] != turno) && (board[x1][y1] != ' ')){
+            x1 = (char) x1 - 65;
+            y1 = y1 - 1;
+
+            printf("-%d %d-\n",(char)x1,y1);
+            
+            
+
+             if ( (x1 > 7) || (x1 < 0) || (y1 > 7) || (y1 < 0) ){
+
+                printf("fuori dalla scacchiera \n");    
+                flag = 1;          
+
+            }
+            else{
+
+                if( (identificaPezzo(board[x1][y1])[1] != turno) && (board[x1][y1] != ' ')){
                 printf("Usa un tuo pezzo \n");       
                 flag = 1;       
             }else if(board[x1][y1] == ' '){
@@ -77,11 +103,6 @@ int main(int argc, char *argv[]){
 
             }
 
-             if ( (x1 > 7) || (x1 < 0) || (y1 > 7) || (y1 < 0) ){
-
-                printf("fuori dalla scacchiera \n");    
-                flag = 1;          
-
             }
 
         } while (flag ); 
@@ -90,14 +111,26 @@ int main(int argc, char *argv[]){
            flag = 0;
 
            printf("%s inserisci pos finale x y: ",(turno ? PLR_TWO : PLR_ONE));
-           scanf("%d %d",&x2,&y2);
-        
+           scanf("%c %d",&x2,&y2);
+           fflush(stdin);
+
+            x2 =(char) x2 - 65;
+            y2 = y2 - 1;     
             
-            if( (board[x2][y2] != ' ')  ){
+           
+            if ( (x2 > 7) || (x2 < 0) || (y2 > 7) || (y2 < 0) ){
+
+                printf("fuori dalla scacchiera \n");    
+                flag = 1;          
+
+            }
+            else{
+
+                 if( (board[x2][y2] != ' ')  ){
 
                 if( ( (turno == identificaPezzo(board[x2][y2])[1]) ) ){
                     printf("\n%c un tuo pezzo\n",138);
-                    printf("\n%d %d\n",identificaPezzo(board[x2][y2])[1],turno);
+                    printf("\n pezzo %d  turno %d\n",identificaPezzo(board[x2][y2])[1],turno);
                     flag = 1;
 
                 }
@@ -107,10 +140,7 @@ int main(int argc, char *argv[]){
                     printf("pezzo catturato %s %c  \n",(turno ? COLOR_ONE : COLOR_TWO ), identificaPezzo(board[x2][y2])[0] );  
 
                 }
-            }else  if ( (x2 > 7) || (x2 < 0) || (y2 > 7) || (y2 < 0) ){
-
-                printf("fuori dalla scacchiera \n");    
-                flag = 1;          
+            }
 
             }
 
@@ -120,46 +150,74 @@ int main(int argc, char *argv[]){
                       if(!checkTorre(board,x1,x2,y1,y2)){
 
                             printf(" \e[0m mossa non valida \n"); 
-                            turno = !turno;
                             flag = 1;
                     }
 
                     break;
                 case 'C':
                     printf("Cavallo");
+                    
+                    if(!checkCavallo(board,x1,x2,y1,y2)){
+                        flag = 1;
+                        printf("\e[0m mossa non valida %d \n",flag);
+                    }
                     break;
                 case 'A':
                     printf("Alfiere");
                      if(!checkAlfiere(board,x1,x2,y1,y2)){
 
-                        turno = !turno;
                         flag = 1;
-                        printf(" \e[0m mossa non valida %d %d \n",flag,turno); 
+                        printf(" \e[0m mossa non valida %d \n",flag);  //DEBUG flag turno
+
                     }
                     break;
                 case 'R':
                     printf("Re");
+                    if( !checkRe(board,x1,x2,y1,y2) ){
+
+                        flag = 1;
+                        printf(" \e[0m mossa non valida %d \n",flag);  //DEBUG flag turno
+
+                    }
                     break;
                 case 'D':
                     printf("Regina");
+                    printf(" \e[0m check %d %d \n", checkAlfiere(board,x1,x2,y1,y2)  , checkTorre(board,x1,x2,y1,y2)  ); //DEBUG
+
+                    if( !( ( !checkAlfiere(board,x1,x2,y1,y2) && checkTorre(board,x1,x2,y1,y2) ) || ( checkAlfiere(board,x1,x2,y1,y2) && !checkTorre(board,x1,x2,y1,y2) ) )){
+
+                        printf(" \e[0m mossa non valida %d %d \n", checkAlfiere(board,x1,x2,y1,y2)  , checkTorre(board,x1,x2,y1,y2)  ); 
+
+                        flag = 1;
+
+                    }
                     break;
                 case 'p':
                     printf("pedone");
+                    if( !checkPedone(board,x1,x2,y1,y2) ){
+
+                        flag = 1;
+                        printf(" \e[0m mossa non valida %d \n",flag);  //DEBUG flag turno
+
+                    }
+
                     break;
                 default:
                     printf("sconosciuto");
             }  
             
-            if (flag==1)
+            if (flag==1){
+                turno = !turno;
                 break;
+            }
 
         } while ( flag ); 
 
         delay(5);
    
-        printf( "\e[1;1H\e[2J"); //CLS
+        //printf( "\e[1;1H\e[2J"); //DEBUG CLS 
 
-        printf("%d\n",flag);
+        printf("# flag = %d#\n",flag); //DEBUG
 
         if(!flag){
 
@@ -183,11 +241,11 @@ int main(int argc, char *argv[]){
 void printMatrice(int board[SIZE][SIZE]){
 
     int b = 0;
-    char *t = "";
+    char *t ;
 
-    for (int y = 0 ; y <= SIZE -1 ; y++){
+    for (int y = 0 ; y < SIZE ; y++){
 
-        for (int x = 0 ; x <= SIZE -1 ; x++){
+        for (int x = 0 ; x < SIZE ; x++){
 
             if (board[x][y]>126){
 
@@ -203,21 +261,23 @@ void printMatrice(int board[SIZE][SIZE]){
 
             if((x+y)%2==0){
 
-                printf ("%s\e[40m %c \e[0m",t, b); //coloreTesto coloreSfondoNero 
+                printf ("%s\e[40m %c \e[0m",t,b); // coloreTesto coloreSfondoNero 
 
             }
             else{
 
-                printf ("%s\e[47m %c \e[0m", t,b); //coloreTesto coloreSfondoBianco 
+                printf ("%s\e[47m %c \e[0m", t,b); // coloreTesto coloreSfondoBianco 
 
             }
+            
 
         }
-
+        printf ("\e[0m %c \e[0m",y+49);
         printf("\n");
         
         
     }
+    printf(" A  B  C  D  E  F  G  H  \n");
 
 };
 
@@ -303,10 +363,15 @@ int checkTorre(int board[SIZE][SIZE], int x1,int x2,int y1,int y2){
      */
             int cx = x2 - x1;
             int cy = y2 - y1;
-            int out = 1;
+            int out = 0;
 
             //la torre si muove orizzontalmente o verticalmente deve risultare o cx = 0 o cy = 0 altrimenti non è una mossa valida
             // poi controllare se in mezzo al percorso ci sono dei pezzi non importa quali [x1,y2] o [y1,y2] estremi esclusi
+
+            
+            if( (!(cx == 0) && (cy == 0)) || ( (cx == 0) && !( cy == 0 ) )  ){
+                out = 1;
+            }
 
             if( cx == 0 ){
                 if( y1 < y2 )
@@ -354,6 +419,7 @@ int checkTorre(int board[SIZE][SIZE], int x1,int x2,int y1,int y2){
 
             } // fine torre
 
+
             return out;
 }
 
@@ -366,20 +432,25 @@ int checkAlfiere(int board[SIZE][SIZE], int x1,int x2,int y1,int y2){
             int out = 0;            
             //controllo movimento giusto
 
-            printf(" %d %d %d", cx, cy, abs(cx/cy));   
+            printf("| %d|", abs(cx/cy));   //DEBUG
             
             if( abs(cx/cy) == 1){
+
                 out = 1;
+
                 if(x1 < x2){
-                    for(int k = x1 + 1 ; k < x2 ; k++) 
-                        if( board[k][y1 + k + 1] != ' ' ){ // 3,5 - 
+                    for(int k = 1 ; k < abs(cx) ; k++)  
+                        if( board[x1 + k][y1 - k ] != ' ' ){ 
+
                             out = 0;
                         }
                 }
                 else{
-                    for(int k = x2 + 1 ; k < x1 ; k++)  
-                        if( board[k][y2 - k] != ' ' ){   
+                    for(int k = 1; k < abs(cx)  ; k++)   
+                        if( board[x2 +k][y2 + k] != ' ' ){  
+
                             out = 0;
+
                         }
                 }
             }
@@ -391,6 +462,58 @@ int checkAlfiere(int board[SIZE][SIZE], int x1,int x2,int y1,int y2){
             return out;
 }
 
+int checkCavallo(int board[SIZE][SIZE],int x1,int x2,int y1,int y2){
+
+    int pattern[5][5] = {   {0,1,0,1,0},
+                            {1,0,0,0,1},
+                            {0,0,0,0,0},
+                            {1,0,0,0,1},
+                            {0,1,0,1,0} };
+
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+
+    int out = pattern[2+dx][2+dy];
+
+    printf("%d",out);
+
+    return out;
+}
+
+
+int checkRe(int board[SIZE][SIZE],int x1,int x2,int y1,int y2){
+
+    int pattern[3][3] = {   {1,1,1},
+                            {1,0,1},
+                            {1,1,1} };
+
+    int dx = x2 - x1; //DEBUG 0 -1 -> 1,0
+    int dy = y2 - y1;
+
+    int out = pattern[1+dx][1+dy];
+
+    printf("%d",out);
+
+    return out;
+}
+
+
+int checkPedone(int board[SIZE][SIZE],int x1,int x2,int y1,int y2){
+    //da fare
+    int pattern[3][3] = {   {1,0,1},
+                            {1,1,1},
+                            {0,1,0}
+                        };
+
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+
+    int out = pattern[1+dx][abs(dy)];
+
+    printf("%d",out);
+
+    return out;
+}
 
 /*
 int checkAlfiere(int board[SIZE][SIZE], int x1,int x2,int y1,int y2){}
